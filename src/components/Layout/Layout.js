403bcd1a6dae96946756a2d951b73bb578f4446a
unknown <habib.redissi@gmail.com>
2019-03-29 16:26:32 +0100

    SideDrawer Added functions Open and Close

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/ReactAux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  sideDrawerOpenedHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !this.state.showSideDrawer };
+    });
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar openedDrawer={this.sideDrawerOpenedHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
-export default layout;
+export default Layout;
